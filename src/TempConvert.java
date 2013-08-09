35df9f2d280b4c3fdb6e9677dead846f0c183fa8
martin2387 <cheenote@hotmail.com>
2013-08-09 12:05:42 -0400

    Here is my assignment clyde

--- src/TempConvert.java
+++ src/TempConvert.java
@@ -1,18 +1,19 @@
-import java.until.Scanner;
-public class ThisIsMyClassName 
+import java.util.Scanner;
 
-	public static main(String[] args) {
+public class TempConvert {
+
+	public static void main(String[] args) {
 		// -------INSTRUCTIONS-------
 		// Fix the errors in this class, to create a working program
 		// Hint: there are TEN errors in total
-		sc = new Scanner(System.in);
-		int tempF; tempC;
+		Scanner kbrd = new Scanner(System.in);
+		int tempF, tempC;
 		
-		System.out.print("Enter a temperature in degreez Fahrenheit: ");
-		tempF = sc..nextInt();
-		tempC = (tempF + 32 ) * 5 / 9;
+		System.out.print("Enter a temperature in degrees Fahrenheit: ");
+		tempF = kbrd.nextInt();
+		tempC = (tempF - 32 ) * 5 / 9;
 		
-		System.out.println(tempF + " degrees Fahrenheit equals " tempC + " degrees Celcius.");
+		System.out.println(tempF + " degrees Fahrenheit equals " + tempC + " degrees Celcius.");
 	}
 
 }
